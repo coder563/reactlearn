13956bcf2dfee1ab938119ca07787614c28f3646
Walker563 <walkerml563@gmail.com>
2023-01-28 06:17:09 +0000

    code add

--- reacthook/reacthook.js
+++ reacthook/reacthook.js
@@ -1,1 +1,46 @@
-import 
+function Button(props){
+    console.log('inside Button Function ' )
+    return  (     
+      <button onClick={()=>props.onClickFunction(props.increment)}>
+              {props.increment}
+      </button>
+    )
+  }
+  
+  function Display(props){
+    
+    
+    return <div>{props.msg}</div>
+    
+    
+    
+  }
+  
+  function App(){
+    
+   const [counter,setCounter] = useState(42)
+  
+    function incrementorFunction(incrementvalue){
+        console.log('inside incrementor Function ')
+         setCounter(counter+incrementvalue)
+    }
+      // 
+    
+     return (
+        <div>
+           <Button onClickFunction={incrementorFunction} increment={10}></Button>
+           <Display msg={counter}></Display>        
+        </div>
+     )
+    
+       
+    
+  }
+  
+  ReactDOM.render(
+                <App/>,
+                document.getElementById('mountNode')
+                 
+                 )
+    
+  
